354c695c224032dc0a5b073eae8dcea1ec8e607c
kuei <ee80@aaa.jjj>
2015-06-30 16:16:24 +0800

    first commit System.out.println("hello");

---
+++ proj08/src/proj01/test.java
@@ -0,0 +1,10 @@
+package proj01;
+
+public class test {
+private void ya() {
+	// TODO Auto-generated method stub
+System.out.println("hello");
+System.out.println("hihihi");
+System.out.println("hahaha");
+}
+}
